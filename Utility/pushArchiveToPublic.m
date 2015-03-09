fb304731cb282caaf5b45424cb33a2dea9db39b2
David Meissner <david.meissner@mathworks.com>
2015-03-09 01:55:13 -0700

    adding file to guide pushing archive to public folder

---
+++ Utility/pushArchiveToPublic.m
@@ -0,0 +1,27 @@
+%% Push Archive To Public Folder
+%
+% In order to make demo content developed or maintained using Git available
+% to those who do not use Git, it's beneficial to place a .zip archive of
+% the material in your public folder.
+%
+% By David Meissner
+% Copyright 2015, The MathWorks, Inc.
+%% Assumptions
+%
+% The version of the material that you want to archive is the latest
+% commit on the master branch
+%
+%% Checkout the master
+!git checkout master
+
+%% Create the Archive
+timeStamp = char(datetime('now','Format','yyyy-MM-dd HH:mm:ss'));
+thisProject = slproject.getCurrentProject;
+projectName = thisProject.Name;
+fileName = [projectName,timeStamp];
+% !git archive -o fileName.zip HEAD
+
+%% Copy/Paste the Archive to your public
+
+myPublic = '\\mathworks\AH\Public\David_Meissner\sharedDemos\CoreDemoTemplateGit';
+% !copy fileName myPublic
