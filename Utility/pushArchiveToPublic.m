526fd8d6015702190b5b02fb8153905b34b7206b
David Meissner <david.meissner@mathworks.com>
2015-03-13 17:24:12 -0700

    adding pushArchiveToPublic.m

--- Utility/pushArchiveToPublic.m
+++ Utility/pushArchiveToPublic.m
@@ -1,4 +1,4 @@
-%% Push Archive To Public Folder
+%% Archive the Current HEAD can copy it to you public folder
 %
 % In order to make demo content developed or maintained using Git available
 % to those who do not use Git, it's beneficial to place a .zip archive of
@@ -8,20 +8,33 @@
 % Copyright 2015, The MathWorks, Inc.
 %% Assumptions
 %
-% The version of the material that you want to archive is the latest
-% commit on the master branch
+% The version of the material that you want to archive is the current HEAD
 %
-%% Checkout the master
-% !git checkout master
-
-%% Create the Archive
-timeStamp = char(datetime('now','Format','yyyy-MM-dd_HH:mm:ss'));
+%% Set Defaults
 thisProject = slproject.getCurrentProject;
 projectName = thisProject.Name;
-fileName = [projectName,timeStamp]
-% >git archive -o fileName.zip HEAD
+myPublicFolder = '\\mathworks\AH\Public\David_Meissner\sharedDemos\CoreDemoTemplateGit';
 
-%% Copy/Paste the Archive to your public
+%% Prompt for User Input
+prompt = {['File name for the .zip file: default file name is ', projectName, '.zip'], ['Path to Public Folder: default path is', myPublicFolder]};
+dlg_title = 'Archive';
+num_lines = 1;
+def = {'20','hsv'};
+answer = inputdlg(prompt,dlg_title,num_lines,def);
+% Use the defaults unless something else was entered
+if ~isempty(answer{1})
+    fileName = answer{1};
+else
+    fileName = projectName;
+end
+if ~isempty(answer{2})
+    folderName = answer{2};
+else
+    folderName = myPublicFolder;
+end
 
-myPublic = '\\mathworks\AH\Public\David_Meissner\sharedDemos\CoreDemoTemplateGit'
-% >copy fileName myPublic
+%% Create the Archive
+eval(['!git archive -o ', fileName, '.zip HEAD'])
+
+%% Copy/Paste the Archive to your public folder
+eval(['copy ', fileName, '.zip ', folderName, ' /Y'])
